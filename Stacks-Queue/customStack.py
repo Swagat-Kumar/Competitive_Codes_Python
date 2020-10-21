d215715d24dffeb4b7ffe7390abd545d4aa568a0
Swagat-Kumar <swagat.kumar.code@gmail.com>
2020-10-21 23:38:31 +0530

    Stack with increment operation LeetCode

---
+++ Stacks-Queue/customStack.py
@@ -0,0 +1,26 @@
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.maxS = maxSize
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if len(self.stack) == self.maxS:
+            return
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        if len(self.stack) != 0:
+            return self.stack.pop()
+        return -1
+
+    def increment(self, k: int, val: int) -> None:
+        for i in range(min(k, len(self.stack))):
+            self.stack[i] += val
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
