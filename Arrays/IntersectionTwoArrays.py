0c17a0e3640dcae40662f95420001125f502606a
Swagat-Kumar <swagat.kumar.code@gmail.com>
2020-11-09 21:58:46 +0530

    Small Change LeetCode

--- Arrays/IntersectionTwoArrays.py
+++ Arrays/IntersectionTwoArrays.py
@@ -1,5 +1,5 @@
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def intersection(self, nums1, nums2):
         aux = []
         for n in nums1:
             if n in aux:
