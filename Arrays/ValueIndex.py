6ca41fdf60534c18604843b90da4ce136a1e4cd2
Swagat-Kumar <swagat.kumar.code@gmail.com>
2020-11-22 18:11:51 +0530

    Value Equal to Index GFG

--- Arrays/ValueIndex.py
+++ Arrays/ValueIndex.py
@@ -1,25 +1,9 @@
 class Solution:
-    def pivotIndex(self, nums) -> int:
-        if len(nums) == 0:
-            return -1
-        if len(nums) == 1:
-            return 0
 
-        def sumAux(nos):
-            if len(nos) < 1:
-                return []
-            aux = [0]*len(nos)
-            aux[0] = nos[0]
-            for i in range(1, len(nos)):
-                aux[i] = aux[i-1]+nos[i]
-            return aux
-        fSum = sumAux(nums)
-        bSum = sumAux(nums[::-1])[::-1]
-        if bSum[1] == 0:
-            return 0
-        for i in range(1, len(nums)-1):
-            if fSum[i-1] == bSum[i+1]:
-                return i
-        if fSum[-2] == 0:
-            return len(nums)-1
-        return -1
+    def valueEqualToIndex(self, arr, n):
+        # code here
+        self.count = []
+        for i in range(len(arr)):
+            if arr[i] == i+1:
+                self.count.append(i+1)
+        return self.count
