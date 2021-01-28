7ba11a9ab9798bbff2aeae2245026c37b229ff53
Swagat-Kumar <swagat.kumar.code@gmail.com>
2021-01-28 17:35:41 +0530

    repeated String occurence of character a

---
+++ BasicsOfStringManipulation/repeatedSubstrings.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+
+
+def repeatedString(s, n):
+    main = s.count('a')*(n//len(s))
+    remainder = s.count('a', 0, n % len(s))
+    return main+remainder
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
