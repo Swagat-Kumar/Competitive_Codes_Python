5c4a92f8e93091db4da33ef3d2c17c630a53369c
Swagat-Kumar <swagat.kumar.code@gmail.com>
2021-03-04 18:26:52 +0530

    Roman To Integer Conversion LeetCode

---
+++ APAS/RomanToInteger.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        num = 0
+        for i in range(len(s)-1):
+            if dic[s[i]] < dic[s[i+1]]:
+                num -= dic[s[i]]
+            else:
+                num += dic[s[i]]
+        num += dic[s[-1]]
+        return num
