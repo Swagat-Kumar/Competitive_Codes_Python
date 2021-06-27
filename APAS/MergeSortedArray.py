92729560e83fd37f14c003a61e506e9e2f15b1be
Swagat-Kumar <swagat.kumar.code@gmail.com>
2021-06-27 12:59:41 +0530

    Merge Two Sorted Arrays LeetCode

---
+++ APAS/MergeSortedArray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, nums1, m: int, nums2, n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m-1
+        j = n-1
+        k = len(nums1)-1
+        while k >= 0:
+            if i < 0:
+                nums1[k] = nums2[j]
+                j -= 1
+            elif j < 0:
+                nums1[k] = nums1[i]
+                i -= 1
+            elif nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
